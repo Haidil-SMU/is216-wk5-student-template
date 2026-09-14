797a2a3a2e1f30e03c2eb2ab66cfe82ed996cd3a
Haidil-SMU <haidil.ms.2025@computing.smu.edu.sg>
2026-09-14 09:21:39 +0800

    ex 4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,9 +1,19 @@
 <script setup>
 import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
+const itemToAdd = ref("")
 
 // TODO: Add Code Here
 
+function deleteItem(item) {
+    const index = items.value.indexOf(item)
+    items.value.splice(index, 1)
+}
+
+function addItem(itemToAdd) {
+    items.value.push(itemToAdd);
+    console.log(items);
+}
 
 </script>
 
@@ -11,6 +21,16 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <div>
+        <ul>
+            <li v-for="item in items">{{item}}
+                <button type="button" @click="deleteItem(item)">Delete</button>
+            </li>
+
+        </ul>
+        <input type="text" v-model="itemToAdd">
+        <button type="button" @click="addItem(itemToAdd)">Add</button>
+    </div>
 
    
 </template>
